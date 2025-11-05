5ad64b8eac7c89c2a647e26aa61b611d41994234
Nhat Minh <nhatminh@gmail.com>
2025-11-05 11:51:21 +0700

    application

--- TowerDefense/TowerDefense/Application.h
+++ TowerDefense/TowerDefense/Application.h
@@ -1,3 +1,95 @@
+#pragma once
 
+#include "ResourceHolder.h"
+#include "ResourceIdentifiers.h"
+#include "Player.h"
+#include "StateStack.h"
+
+#include <SFML/Graphics/RenderWindow.hpp>
+
+class Application
+{
+public:
+    Application(); // Constructor: Initializes the application
+    void run(); // Main game loop entry point
+
+private:
+    // Handles all user inputs (keyboard, mouse, etc.)
+    void processInput();
+
+    // Updates game logic based on elapsed time (delta time)
+    void update(sf::Time dt);
+#pragma once
+
+#include "ResourceHolder.h"
+#include "ResourceIdentifiers.h"
+#include "Player.h"
+#include "StateStack.h"
+
+#include <SFML/Graphics/RenderWindow.hpp>
+
+    class Application
+    {
+    public:
+        Application(); // Constructor: Initializes the application
+        void run(); // Main game loop entry point
+
+    private:
+        // Handles all user inputs (keyboard, mouse, etc.)
+        void processInput();
+
+        // Updates game logic based on elapsed time (delta time)
+        void update(sf::Time dt);
+
+        // Draws everything to the screen
+        void render();
+
+        // Registers all possible game states into the state stack
+        void registerStates();
+
+        sf::RenderWindow mWindow;
+        TextureHolder mTextures;
+        FontHolder mFonts;
+        Player mPlayer;
+        int mVictoryStars;
+        StateStack mStateStack;
+
+        MusicState currentMusic = MusicState::None;
+        SoundBufferHolder mSoundBuffers;
+        MusicHolder mMusics;
+
+        float gameSpeed = 1.0f;
+
+        bool isMusicOn = true;
+        bool isSoundOn = true;
+
+    private:
+        sf::Sound mClickSound;
+    };
+    // Draws everything to the screen
+    void render();
+
+    // Registers all possible game states into the state stack
+    void registerStates();
+
+    sf::RenderWindow mWindow;
+    TextureHolder mTextures;
+    FontHolder mFonts;
+    Player mPlayer;
+    int mVictoryStars;
+    StateStack mStateStack;
+
+    MusicState currentMusic = MusicState::None;
+    SoundBufferHolder mSoundBuffers;
+    MusicHolder mMusics;
+
+    float gameSpeed = 1.0f;
+
+    bool isMusicOn = true;
+    bool isSoundOn = true;
+
+private:
+    sf::Sound mClickSound;
+};
 
 
